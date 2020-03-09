634fcb4193748dbfafc1cf3b2724b41edc71b11c
Asaf Meir <asi.meir@gmail.com>
2020-03-09 06:32:49 +0200

    update our menu-item to navigate withRrouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { Route, Link, Switch } from 'react-router-dom'
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.component';
+const HomePage = () => {
+  
+  return (
+    <div>
+      <h1>HOME PAGE</h1>
+    </div>
+  );
+};
+
+const PicklesPage = () => (
+  <div>
+    <h1>חמוצים</h1>
+  </div>
+);
+
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/pickles' component={PicklesPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,35 +12,40 @@ class Directory extends React.Component {
             [{
                 title:'חמוצים',
                 imageUrl: 'https://images.unsplash.com/photo-1582536446621-17e0235b514a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
-                id: 1
+                id: 1,
+                linkUrl: 'pickles'
             },
             {   title: 'פיצוחים',
                 imageUrl: 'https://images.unsplash.com/photo-1582173649515-0f9582d58e60?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=769&q=80',
-                id: 2
+                id: 2,
+                linkUrl: ''
             },
             {
                 title: 'שמן זית',
                 imageUrl:'https://images.unsplash.com/photo-1552592074-ea7a91b851b3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80',
-                id: 3
+                id: 3,
+                linkUrl: ''
             }, 
             {
                 title:'פירות',
                 imageUrl:'https://images.unsplash.com/photo-1511546865855-fe4788edf4b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
                 size: 'large',
-                id: 4
+                id: 4,
+                linkUrl: ''
             },
             {   title:'ירקות',
                 imageUrl:'https://images.unsplash.com/photo-1557844352-761f2565b576?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
                 size: 'large',
-                id: 5
+                id: 5,
+                linkUrl: ''
             }]
         }
     }
     render() {
         return (
             <div className='directory-menu'>
-                {this.state.sections.map(({title, imageUrl, id, size}) => (
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                {this.state.sections.map(({id,...otherSectionProps }) => (
+                    <MenuItem key={id} {...otherSectionProps} />
                     ))}
                 </div>
         );
