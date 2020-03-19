15f6afcca5573ef79067b7d0b13f49de14f75e44
Asaf Meir <asi.meir@gmail.com>
2020-03-19 10:14:30 +0200

    updatong our app with a shop page, collection preview component and collection item component fixing compiling problem

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import SHOP_DATA from './shop.data.js'
 
+import CollectionPreview from '../../components/collection-preview/collection-preview.comopnent'
+
 class ShopPage extends React.Component {
     constructor(props) {
         super(props);
@@ -11,10 +13,19 @@ class ShopPage extends React.Component {
         };
     }    
 
-
     render() {
-        return <div>SHOP PAGE</div>
-    }
+        const {collections} = this.state;
+        return (<div className='shop-page'>
+            {
+                collections.map(({id, ...otherCollectionProps }) => (
+                    <CollectionPreview key={id} {...otherCollectionProps} />
+                ))
+            }
+            
+            
+            
+            </div>);
+            }
 }
 
 export default ShopPage;
