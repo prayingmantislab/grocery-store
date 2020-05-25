2d8e15168641f0248d6391301ac9dadb18917bc6
Asaf Meir <office@elpuravida.co.il>
2020-05-25 22:24:42 +0300

    updated shop data with new data updadted cart utils to group

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({ children, ...otherProps }) => (
-    <button className='custom-button' {...otherProps}>
+const CustomButton = ({ children, isGoogleSingIn, inverted, ...otherProps }) => (
+    <button
+     className={`${inverted ? 'inverted' : ''} ${
+         isGoogleSingIn ? 'google-sign-in' : ''
+    } custom-button`} 
+    {...otherProps}
+    >
         {children}
     </button>
 );
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,9 @@
 import CartActionTypes from './cart.types'
+import { addItemToCart } from './cart.utils'
 
 const INTIAIL_STATE = {
-    hidden: true
+    hidden: true,
+    cartItems: []
 };
 
 const cartReducer = (state = INTIAIL_STATE, action) => {
@@ -10,7 +12,12 @@ const cartReducer = (state = INTIAIL_STATE, action) => {
             return {
                 ...state,
                 hidden: !state.hidden
-        }
+        };
+        case CartActionTypes.ADD_ITEM:
+            return {
+                ...state,
+                cartItems: addItemToCart(state.cartItems, action.payload)
+            }
     default: 
         return state;
     }
